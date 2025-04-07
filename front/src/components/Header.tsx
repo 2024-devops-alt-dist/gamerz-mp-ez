00c7af9222fbcfef2b02364dcbf47bd8b2024eab
elena <zianielena@gmail.com>
2025-04-07 11:10:04 +0200

    fix: header css

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
     return (
         <nav className="navbar navbar-expand-lg mb-5">
             <div className="container-fluid">
-                <NavLink to="/" className="navbar-brand fs-3 m-0">Gamerz</NavLink>
+                <NavLink to="/" className="navbar-brand fs-3 m-0 text-light">Gamerz</NavLink>
 
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -12,8 +12,8 @@ function Header() {
 
                 <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
                     <div className="navbar-nav">
-                        <NavLink to="/register" className="nav-link">Inscription</NavLink>
-                        <NavLink to="/login" className="nav-link">Connexion</NavLink>
+                        <NavLink to="/register" className="nav-link text-light">Inscription</NavLink>
+                        <NavLink to="/login" className="nav-link text-light">Connexion</NavLink>
                     </div>
                 </div>
             </div>
